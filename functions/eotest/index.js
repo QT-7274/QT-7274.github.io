b3be38bde9c33340d8312cf21d9545d0db480792
QT-7274 <2531376773@qq.com>
2025-06-02 20:20:31 +0800

    add: fetch

--- functions/eotest/index.js
+++ functions/eotest/index.js
@@ -1,13 +1,14 @@
 export async function onRequest({ request }) {
-  // 获取客户端支持的图片类型
-  const accept = request.headers.get('Accept');
-  const option = { eo: { image: {} } };
+//   // 获取客户端支持的图片类型
+//   const accept = request.headers.get('Accept');
+//   const option = { eo: { image: {} } };
   
-  // 检查客户端是否支持 WebP 格式的图片,若不支持响应原图
-  if (accept && accept.includes('image/webp')) {
-    option.eo.image.format = 'webp';
-  }
+//   // 检查客户端是否支持 WebP 格式的图片,若不支持响应原图
+//   if (accept && accept.includes('image/webp')) {
+//     option.eo.image.format = 'webp';
+//   }
   
-  const response = await fetch(request, option);
+//   const response = await fetch(request, option);
+const response = await fetch(request);
   return response;
 }
