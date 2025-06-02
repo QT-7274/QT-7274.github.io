ce05f7efbbd049cfb72f47e83e6cf321db861464
QT-7274 <2531376773@qq.com>
2025-06-02 20:37:18 +0800

    feat: add webp

--- functions/eotest/index.js
+++ functions/eotest/index.js
@@ -4,9 +4,9 @@ export async function onRequest({ request }) {
     const option = { eo: { image: {} } };
 
     // 检查客户端是否支持 WebP 格式的图片,若不支持响应原图
-    if (accept && accept.includes('image/webp')) {
+    // if (accept && accept.includes('image/webp')) {
       option.eo.image.format = 'webp';
-    }
+    // }
 
   const response = await fetch('http://drogbaqu.qcdntest.cn/drogbaqu/img/drogba.jpg', option);
   
