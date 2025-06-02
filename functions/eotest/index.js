45fcf78018d9657e985304bd98d28739e8cb90f4
QT-7274 <2531376773@qq.com>
2025-06-02 20:07:30 +0800

    fix: change eotest

--- functions/eotest/index.js
+++ functions/eotest/index.js
@@ -1,10 +1,7 @@
-export  async function onRequest({request}) {
-  const response = await fetch('http://drogbaqu.qcdntest.cn/drogbaqu/img/drogba.jpg')
+export async function onRequest({ request }) {
+  const response = await fetch(
+    'http://drogbaqu.qcdntest.cn/drogbaqu/img/drogba.jpg'
+  );
 
-  return new Response(response, {
-    headers: {
-      'content-type': 'application/json; charset=UTF-8',
-      'Access-Control-Allow-Origin': '*',
-    },
-  });
+  return response;
 }
