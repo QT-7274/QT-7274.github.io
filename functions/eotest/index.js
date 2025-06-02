82c277146105f629299a34db27e993319723abe3
QT-7274 <2531376773@qq.com>
2025-06-02 20:12:34 +0800

    feat: change onRequest

--- functions/eotest/index.js
+++ functions/eotest/index.js
@@ -1,7 +1,13 @@
 export async function onRequest({ request }) {
-  const response = await fetch(
-    'http://drogbaqu.qcdntest.cn/drogbaqu/img/drogba.jpg'
-  );
-
+  // 获取客户端支持的图片类型
+  const accept = request.headers.get('Accept');
+  const option = { eo: { image: {} } };
+  
+  // 检查客户端是否支持 WebP 格式的图片,若不支持响应原图
+  if (accept && accept.includes('image/webp')) {
+    option.eo.image.format = 'webp';
+  }
+  
+  const response = await fetch(request, option);
   return response;
 }
